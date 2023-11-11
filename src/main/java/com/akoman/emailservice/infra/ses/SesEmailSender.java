925aaa75075fd3cc5c4a3215395fde9b05851d46
Vinicius Silva <xdenik13@gmail.com>
2023-11-10 21:13:18 -0300

    Camada de infraestrutura finalizada e identação corrigida

--- src/main/java/com/akoman/emailservice/infra/ses/SesEmailSender.java
+++ src/main/java/com/akoman/emailservice/infra/ses/SesEmailSender.java
@@ -3,10 +3,11 @@ package com.akoman.emailservice.infra.ses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.akoman.emailservice.adapters.EmailSenderGateway;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.model.*;
+import com.akoman.emailservice.adapters.EmailSenderGateway;
+import com.akoman.emailservice.core.exceptions.EmailServiceException;
+import com.amazonaws.AmazonServiceException;
 
 @Service
 public class SesEmailSender implements EmailSenderGateway {
@@ -22,10 +23,12 @@ public class SesEmailSender implements EmailSenderGateway {
 	public void sendEmail(String to, String subject, String body) {
 		SendEmailRequest request = new SendEmailRequest().withSource("joaovinicius.210@gmail.com").withDestination(new Destination().withToAddresses(to)).withMessage(new Message()
 				.withSubject(new Content(subject)).withBody(new Body().withText(new Content(body))));
-	};
+
 	try {
 		this.amazonSimpleEmailService.sendEmail(request);
 	} catch (AmazonServiceException exception) { 
-		throw new EmailServiceException("Failure while sending email");
+		throw new EmailServiceException("Failure while sending email", exception);
+		}
 	}
 }
+
